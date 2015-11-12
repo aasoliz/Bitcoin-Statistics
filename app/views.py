f3bff4e183ece5748bef82381a4896429f06328a
Andrea Soliz <and.sol17@gmail.com>
2015-11-11 22:22:55 -0600

    queries depend on scope

--- app/views.py
+++ app/views.py
@@ -22,7 +22,8 @@ def index():
     # Create Month, Day, Hour
     month = Aggregate(month=date.strftime("%B"), month_number=time_m, average_buy=-1, average_sell=-1)
     day = Day(month=date.strftime("%B"), month_number=time_m, average_buy=-1, average_sell=-1, parent_month=month, day_number=time_d)
-    hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1)
+    hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1, hour=day)
+
 
     db.session.add(month)
     db.session.add(day)
@@ -32,13 +33,12 @@ def index():
     buyHour(hour)
     sellHour(hour)
 
-  if month:
+  elif month:
     day = Day.query.filter_by(day_number=time_d, month_id=month.id).first()
-    print "hadysdf %r" % day
 
     if day is None:
       day = Day(month=date.strftime("%B"), month_number=time_m, average_buy=-1, average_sell=-1, parent_month=month, day_number=time_d)
-      hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1)
+      hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1, day_id=day.id)
 
       db.session.add(day)
       db.session.add(hour)
@@ -47,12 +47,11 @@ def index():
       buyHour(hour)
       sellHour(hour)
 
-    if day:
-      hour = Hours.query.filter_by(hour_number=time_h).first()
-      print "hourasdf %r ajkdfl %r jkasdljf %r" % (hour, day.id, time_h)
+    elif day:
+      hour = Hours.query.filter_by(hour_number=time_h, day_id=day.id).first()
 
       if hour is None:
-        hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1)
+        hour = Hours(hour_number=time_h, buy_price=-1, sell_price=-1, day_id=day.id)
 
         db.session.add(hour)
         db.session.commit()
@@ -62,12 +61,8 @@ def index():
 
   months = Aggregate.query.filter_by(discriminator='aggregate')
 
-  hours = Hours.query.all()
-  print "hours %r" % hours
+  days = Day.query.filter_by(day_number=time_d, month_id=month.id)
 
-  days = Day.query.all()
-  print "days %r" % days
-
-  print "months %r" % months
+  hours = Hours.query.filter_by(hour_number=time_h, day_id=day.id)
 
   return render_template('index.html', title='Home', hours=hours, days=days, months=months)
